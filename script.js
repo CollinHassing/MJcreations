d1612cfa2f13d8cb1347e61880a50631d9221a9e
Collin <Collinhassing@gmail.com>
2017-07-29 17:27:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,11 @@ $(document).ready(function() {
 	//Main IMG Modal
 var modal = document.getElementById('imgModal');
 var innerImg = document.getElementById('innerImg');
-
-$('img').on('click', function () {
-    modal.style.display = "block";
-	innerImg.src = this.src;
-});
-
-
 modal.onclick = function() { 
     modal.style.display = "none";
 	}
+$('img').on('click', function () {
+    modal.style.display = "block";
+	innerImg.src = this.src;
+	});
 }
