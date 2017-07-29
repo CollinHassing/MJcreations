5b8d639091824b53491784ebfcce360506525d56
Collin <Collinhassing@gmail.com>
2017-07-29 17:25:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,13 @@ $(document).ready(function() {
 var modal = document.getElementById('imgModal');
 var innerImg = document.getElementById('innerImg');
 
-$('img').on('click', function () {
+$('img').on('click tap', function () {
     modal.style.display = "block";
 	innerImg.src = this.src;
 });
-
-$('img').on('touchstart', function () {
-    modal.style.display = "block";
-	innerImg.src = this.src;
-});
-
 
 
 modal.onclick = function() { 
     modal.style.display = "none";
-}
+	}
 }
