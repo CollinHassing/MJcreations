f79a86f962090786373f314e796e6e6195ecda0b
Collin <Collinhassing@gmail.com>
2017-07-29 16:14:28 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+//Main IMG Modal
+var modal = document.getElementById('imgModal');
+var innerImg = document.getElementById('innerImg');
+
+modal.onclick = function() { 
+    modal.style.display = "none";
+}
+
+$('img').on('click', function () {
+    modal.style.display = "block";
+	innerImg.src = this.src;
+});
