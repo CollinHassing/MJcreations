49d527acde4d94043d912bd0baa194e4d4e22842
Collin <Collinhassing@gmail.com>
2017-07-29 16:46:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ $(document).ready(function() {//Main IMG Modal
 var modal = document.getElementById('imgModal');
 var innerImg = document.getElementById('innerImg');
 
-modal.onclick = function() { 
-    modal.style.display = "none";
-}
-
-$('img').on('click touchend', function () {
+$('img').on('click touchstart', function () {
     modal.style.display = "block";
 	innerImg.src = this.src;
 });
 
+modal.onclick = function() { 
+    modal.style.display = "none";
+	}
 }
